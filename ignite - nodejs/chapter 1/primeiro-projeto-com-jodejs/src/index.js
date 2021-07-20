0cf713dbff4b3cc9ef647487b7220c9b18786f26
jvfq2000 <jvfq2000@gmail.com>
2021-07-19 22:24:05 -0300

    Listando extrato

--- ignite - nodejs/chapter 1/primeiro-projeto-com-jodejs/src/index.js
+++ ignite - nodejs/chapter 1/primeiro-projeto-com-jodejs/src/index.js
@@ -30,4 +30,14 @@ app.post("/account", (request, response) => {
 
 });
 
+app.get("/statement/:cpf", (request, response) => {
+    
+    const {cpf} = request.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    return response.json(customer.statement);
+
+})
+
 app.listen(3333);
