86c0704c51dc03aaf5f2e027f2ec219a409c33f4
jvfq2000 <jvfq2000@gmail.com>
2021-08-03 22:32:05 -0300

    S.O.L.I.D:     Utilizando o Princípio de Desponsabilidade Única (SRP).

--- ignite - nodejs/chapter 2/rentalx/src/repositories/CategoriesRepository.ts
+++ ignite - nodejs/chapter 2/rentalx/src/repositories/CategoriesRepository.ts
@@ -30,7 +30,6 @@ class CategoriesRepository {
 
   findByName(name: string): Category {
     const category = this.categories.find(category => category.name === name);
-
     return category;
   }
 }
