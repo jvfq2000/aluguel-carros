8a2871b2b3a257de106fb55117f9f4f75e26720b
jvfq2000 <jvfq2000@gmail.com>
2021-08-04 22:33:34 -0300

    Continuaçao da aplicaçao:     Criando repositorio de especificaçao.

--- ignite - nodejs/chapter 2/rentalx/src/modules/cars/services/CreateSpecificationService.ts
+++ ignite - nodejs/chapter 2/rentalx/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { SpecificationsRepository } from "../repositories/SpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("TODO");
+  constructor(private specificationsRepository: SpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
 
