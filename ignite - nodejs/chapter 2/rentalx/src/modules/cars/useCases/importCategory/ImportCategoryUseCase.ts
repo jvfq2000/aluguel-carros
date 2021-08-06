2eb8638532d13e8b28f98d27553a07693fdc5213
jvfq2000 <jvfq2000@gmail.com>
2021-08-06 18:09:11 -0300

    Trabalhando com Upload:     Conhecendo o conceito de stream.

--- ignite - nodejs/chapter 2/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ ignite - nodejs/chapter 2/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCse {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async line => {
+      console.log(line);
+    });
   }
 }
 
